42c89d0543cd2784de447ec875e45fab48c7b5a6
Tushti Gulati <1805173@kiit.ac.in>
2022-03-22 07:40:56 +0530

    Added custom card

--- flutter_package/lib/flutter_package.dart
+++ flutter_package/lib/flutter_package.dart
@@ -1,7 +1,102 @@
 library flutter_package;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class BottomCards extends StatefulWidget {
+  final String? cardTitle;
+  final String? cardContent;
+  final IconData? iconOne;
+  final IconData? iconTwo;
+  final IconData? iconThree;
+  final Function()? iconOneOnPressed;
+  final Function()? iconTwoOnPressed;
+  final Function()? iconThreeOnPressed;
+  final Color? iconOneColor;
+  final Color? iconTwoColor;
+  final Color? iconThreeColor;
+  final Color? cardColor;
+  final Color? cardContentColor;
+  final Color? cardTitleColor;
+  final Function()? onTap;
+  final double width;
+  final double height;
+
+  BottomCards(
+      {this.cardTitle = "Dart Package",
+      this.cardContent =
+          "Dart packages help us solve issues and create workarounds for problems without having to write the code ourselves from scratch.",
+      this.iconOne = Icons.refresh_rounded,
+      this.iconTwo = Icons.favorite,
+      this.iconThree = Icons.paste,
+      this.iconOneOnPressed,
+      this.iconTwoOnPressed,
+      this.iconThreeOnPressed,
+      this.iconOneColor = Colors.green,
+      this.iconTwoColor = Colors.red,
+      this.iconThreeColor = Colors.black,
+      this.cardColor = Colors.white,
+      this.cardContentColor = Colors.black,
+      this.cardTitleColor = Colors.purple,
+      this.onTap,
+      this.width = 500.0,
+      this.height = 300.0});
+  @override
+  _BottomCardsState createState() => _BottomCardsState();
+}
+
+class _BottomCardsState extends State<BottomCards> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: widget.width,
+        height: widget.height,
+        child: Card(
+          color: widget.cardColor,
+          child: InkWell(
+            splashColor: Colors.blue.withAlpha(30),
+            onTap: widget.onTap ?? () {},
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: ListTile(
+                title: Text(
+                  widget.cardTitle!,
+                  style: TextStyle(color: widget.cardTitleColor),
+                ),
+                subtitle: Text(
+                  widget.cardContent!,
+                  style: TextStyle(color: widget.cardContentColor),
+                ),
+                trailing: Row(
+                  children: <Widget>[
+                    ButtonBar(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(
+                            widget.iconOne,
+                            color: widget.iconOneColor,
+                          ),
+                          onPressed: widget.iconOneOnPressed ?? () {},
+                        ),
+                        IconButton(
+                          icon:
+                              Icon(widget.iconTwo, color: widget.iconTwoColor),
+                          onPressed: widget.iconTwoOnPressed ?? () {},
+                        ),
+                        IconButton(
+                          icon: Icon(widget.iconThree,
+                              color: widget.iconThreeColor),
+                          onPressed: widget.iconThreeOnPressed ?? () {},
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
